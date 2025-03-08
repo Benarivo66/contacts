5e71da4b2a4f572cc9871603502f6db7722ec785
benarivo66 <iz4comed@yahoo.com>
2025-03-08 11:14:12 +0100

    add a home page route

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+app.get("/", (req, res, next) => {
+    res.send("Welcome to the contacts home page")
+});
 app.use("/contacts", contactsRoute);
 
 const port = process.env.PORT;
