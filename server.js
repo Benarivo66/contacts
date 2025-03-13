99b8d620b174e3c49f1603531e3a11ee3704ffbb
benarivo66 <iz4comed@yahoo.com>
2025-03-13 08:23:58 +0100

    API Documentation added

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const contactsRoute = require("./routes/contact")
 const bodyParser = require("body-parser")
 const cookieParser = require("cookie-parser")
 const cors = require("cors");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(cors());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) 
 
 app.get("/", (req, res, next) => {
     res.send("Welcome to the contacts home page")
---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts',
+    description: 'API documentation',
+  },
+  host: 'localhost:3000',
+  schemes: ['http'],
+};
+
+const outputFile = './swagger.json';
+const endpointsFiles = ['./server.js'];
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  console.log('Swagger JSON generated.');
+});
